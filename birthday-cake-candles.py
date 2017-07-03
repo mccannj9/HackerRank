e08882f6542ce9f29a8009a31d2a71732bc41a36
mccannj9 <mccannj@protonmail.com>
2017-07-03 12:26:02 +0200

    Fixed small error in script before submission

--- birthday-cake-candles.py
+++ birthday-cake-candles.py
@@ -10,11 +10,10 @@ def main():
 
     data = [line.rstrip().split() for line in sys.stdin.readlines()]
     num_candles = int(data[0][0])
-    for x in range(1, num_candles+1):
-        heights = [int(y) for y in data[x]]
-        maximum = max(heights)
-        blown_out = heights.count(maximum)
-        print(blown_out)
+    heights = [int(y) for y in data[1]]
+    maximum = max(heights)
+    blown_out = heights.count(maximum)
+    print(blown_out)
 
 if __name__ == '__main__':
     main()
