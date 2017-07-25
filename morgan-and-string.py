5fc0595ef423d57c1a47fe1df10f73d3711f1448
mccannj9 <mccannj@protonmail.com>
2017-07-25 10:31:35 +0200

    Load data correctly for computing LMS

---
+++ morgan-and-string.py
@@ -0,0 +1,22 @@
+#! /usr/bin/env python
+
+# https://www.hackerrank.com/challenges/morgan-and-a-string
+# Problem name ::: Morgan and a string
+# 
+
+def compare_strings(s1, s2):
+	c1, c2 = 0, 0
+	while True:
+		break
+
+def main():
+
+    import sys
+
+    data = [line.rstrip() for line in sys.stdin.readlines()][1:]
+    for s1,s2 in zip(data[0::2], data[1::2]):
+    	compare_strings(s1,s2)
+
+
+if __name__ == '__main__':
+    main()
